e91a603eb697ae3d2a219c0e445ce106d72c98fa
matty <matty@mattys-MacBook-Pro.local>
2022-08-20 06:19:18 -0500

    initial commit

---
+++ tik-tok-clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tik-tok-clone
+//
+//  Created by matty on 8/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tik-tok-clone/tik_tok_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  tik_tok_cloneApp.swift
+//  tik-tok-clone
+//
+//  Created by matty on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct tik_tok_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
